e28965f9512c46af5df7f9172f5a6d3b5607c297
fahmin.q <fehmi@DESKTOP-6P9HRUK>
2023-08-28 15:27:48 +0400

    28/08/2023 test branch

--- EcoCars_Project.Domain/Entities/Model.cs
+++ EcoCars_Project.Domain/Entities/Model.cs
@@ -4,9 +4,8 @@ namespace EcoCars_Project.Domain.Entities
 {
     public class Model:BaseEntity
     {
-        public int Id { get; set; }
         public string ModelName { get; set; }
-        public int BrandId { get; set; }
-        public Brand brand { get; set; }
+        public Guid BrandId { get; set; }
+        public Brand Brand { get; set; }
     }
 }
--- EcoCars_Project.Persistance/Configuration.cs
+++ EcoCars_Project.Persistance/Configuration.cs
@@ -14,7 +14,7 @@ namespace EcoCars_Project.Persistance
             get
             {
                 ConfigurationManager configurationManager = new();
-                configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../../Presentation/ETicaretAPI.API"));
+                configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../../EcoCars_Project/EcoCars_Project.API"));
                 configurationManager.AddJsonFile("appsettings.json");
 
                 return configurationManager.GetConnectionString("PostgreSQL");
